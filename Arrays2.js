44ec10a4c1c4a77084ad753d8182cd3d21795872
tezu <tezu@fresnou.org>
2023-03-29 10:27:52 -0700

    Return1

--- Arrays2.js
+++ Arrays2.js
@@ -1,5 +1,21 @@
-
+//1
 let nums = [1, 2, 3, 4, 5];
 let newNums = [1, 2];
 console.log('if n =5: ' + nums);
 console.log('if n =2: ' + newNums);
+
+//2
+function countUp(n){
+    if (n <= 0){
+        console.log('Invalid')
+    }
+    else( n >=0);
+    let numberList = [];
+    for (let i=1; i<=n; i++){
+        numberList.push(i);
+    }
+    console.log(numberList)
+}
+countUp(-1);
+
+//3
